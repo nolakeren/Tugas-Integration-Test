a9984ec83586db7def98d2f2d77f9624705123f1
nolakeren <75222453+nolakeren@users.noreply.github.com>
2022-05-16 22:41:15 +0700

    Tugas TE Integration Test

---
+++ TugasIntegrationTest/src/test/java/UserServiceImplTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+        import org.mockito.Mockito;
+        import static org.mockito.Mockito.mock;
+        import static org.mockito.Mockito.verify;
+
+public class UserServiceImplTest {
+
+    User user = Mockito.mock(User.class);
+
+    SecurityService securityService = Mockito.mock(SecurityService.class);
+    UserDAO userDaO = Mockito.mock(UserDAO.class);
+
+    @Test
+
+    public void tesAsPass() throws Exception {
+        UserServiceImpl sut = new UserServiceImpl(userDaO, securityService);
+
+        sut.assignPassword(user);
+        verify(user).getPassword();
+    }
+    @Test
+    public void tesmethod() throws Exception{
+
+        UserServiceImpl sut2 = new UserServiceImpl(userDaO, securityService);
+        sut2.assignPassword(user);
+        verify(userDaO).updateUser(user);
+
+
+    }
+}
